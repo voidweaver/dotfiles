f0735b0712ce985741f1ca71295ff20dd31f8f42
voidweaver <44946758+voidweaver@users.noreply.github.com>
2020-05-14 23:30:52 +0700

    Recolor.

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -793,9 +793,9 @@
 
   ##################################[ context: user@hostname ]##################################
   # Context color when running with privileges.
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=208
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=209
   # Context color in SSH without privileges.
-  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=178
+  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=215
   # Default context color (no privileges, no SSH).
   typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=221
 
